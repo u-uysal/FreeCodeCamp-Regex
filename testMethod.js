ca912b24dab8d124291fe28b09a9efe04d22da9d
ufuk <ufuk.uysl5@gmail.com>
2020-06-20 10:04:27 +0200

    Add comment about case sensitive

--- testMethod.js
+++ testMethod.js
@@ -2,6 +2,8 @@ let myString = "Hello, World!";
 
 let myRegex = /Hello/; // no need to quote marks
 
+// regex is case-sensitive. you have to write exact word. For example; not /HELLO/
+
 let result = myRegex.test(myString);
 
 console.log(result); // true
