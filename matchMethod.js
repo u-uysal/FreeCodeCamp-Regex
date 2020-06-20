769c8a4abc8ce4a85a5d88a04c09b65af8f106b9
ufuk <ufuk.uysl5@gmail.com>
2020-06-20 10:39:57 +0200

    Add "g" flag feature

--- matchMethod.js
+++ matchMethod.js
@@ -15,3 +15,24 @@ ourStr.match(ourRegex);
 'string'.match(/regex/);
 
 /regex/.test('string');
+
+
+
+
+// To search or extract a pattern more than once, you can use the "g" flag
+
+let repeatRegex = /Repeat/g;
+
+testStr.match(repeatRegex);
+
+// Returns ["Repeat", "Repeat", "Repeat"]
+
+
+let twinkleStar = "Twinkle, twinkle, little star";
+
+let starRegex = /twinkle/ig;
+
+let result = twinkleStar.match(starRegex);
+
+
+console.log(result); //[ 'Twinkle', 'twinkle' ] => it is array
