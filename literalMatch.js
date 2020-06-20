b930f45778b0506e781dc01398d132f29f850887
ufuk <ufuk.uysl5@gmail.com>
2020-06-20 13:39:33 +0200

    Match single characters with multiple possibilities

---
+++ literalMatch.js
@@ -0,0 +1,14 @@
+let bgRegex = /b[aiu]g/;
+
+// For example, you want to match "bag", "big", and "bug" but not "bog"
+
+
+let bigStr = "big";
+let bagStr = "bag";
+let bugStr = "bug";
+let bogStr = "bog";
+let bgRegex = /b[aiu]g/;
+bigStr.match(bgRegex); // Returns ["big"]
+bagStr.match(bgRegex); // Returns ["bag"]
+bugStr.match(bgRegex); // Returns ["bug"]
+bogStr.match(bgRegex); // Returns null
