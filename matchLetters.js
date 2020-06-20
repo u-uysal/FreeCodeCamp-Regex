84bbdd2df776382ed489e0e96d57d615407c59ef
ufuk <ufuk.uysl5@gmail.com>
2020-06-20 13:40:07 +0200

    Match Letters of Alphabet

---
+++ matchLetters.js
@@ -0,0 +1,7 @@
+let catStr = "cat";
+let batStr = "bat";
+let matStr = "mat";
+let bgRegex = /[a-e]at/;
+catStr.match(bgRegex); // Returns ["cat"]
+batStr.match(bgRegex); // Returns ["bat"]
+matStr.match(bgRegex); // Returns null
