efe756a4848989eacf0c39a681e723f6f4fb4f5d
ufuk <ufuk.uysl5@gmail.com>
2020-06-21 15:48:28 +0200

    Add closest character

--- matchAll.js
+++ matchAll.js
@@ -7,6 +7,7 @@ shortHand.test(numbers); // Returns true
 longHand.test(varNames); // Returns true
 shortHand.test(varNames); // Returns true
 
+//The closest character class in JavaScript to match the alphabet is \w. This shortcut is equal to [A-Za-z0-9_]
 
 let quoteSample = "The five boxing wizards jump quickly.";
 let alphabetRegexV2 = /\w/g; // Change this line
