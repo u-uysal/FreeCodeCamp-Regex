0043e9e5d78cd7bbb979edf6d9c11ec90c4f2ca1
ufuk <ufuk.uysl5@gmail.com>
2020-06-21 16:14:14 +0200

    Match whitespace

---
+++ whiteSpace.js
@@ -0,0 +1,12 @@
+/* You can search
+for whitespace using\ s, which is a lowercase s.This pattern not only
+ matches whitespace, but also carriage
+return, tab, form feed, and new line characters. */
+
+
+let whiteSpace = "Whitespace. Whitespace everywhere!"
+
+let spaceRegex = /\s/g;
+
+whiteSpace.match(spaceRegex);
+// Returns [" ", " "]
